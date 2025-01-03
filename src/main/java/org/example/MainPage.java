7b23e39fc4659af7eaef81eb12dd7b9ba5c12eb3
Yaroslav Rymarchuk <yaroslav.rymarchuk.qa@ukr.net>
2025-01-03 13:29:48 +0300

    add allure annotations

--- src/main/java/org/example/MainPage.java
+++ src/main/java/org/example/MainPage.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,6 +12,8 @@ import org.testng.Assert;
 
 import java.util.List;
 
+@Epic("Amazon Search")
+@Feature("Search Functionality")
 public class MainTest {
     private WebDriver driver;
     private MainPage mainPage;
@@ -20,6 +23,7 @@ public class MainTest {
 
     // настройка перед тестом
     @BeforeMethod
+    @Step("Setup WebDriver and initialize pages")
     public void setup(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-web-security");
@@ -34,26 +38,72 @@ public class MainTest {
 
     //початок тесту
     @Test
+    @Story("User searches for a book by title")
+    @Description("Проверка поиска книги 'Java' в категории 'Books'")
+    @Severity(SeverityLevel.CRITICAL)
     public void testSearch() {
-        mainPage.open();
+        openMainPage();
 
-        mainPage.setParameter("Books");
+        setParameter();
 
-        mainPage.enterWord("Java");
+        enterWord();
 
-        mainPage.clickOnSearchBtn();
+        clickOnSearchBtn();
 
-        List<Book> books = mainPage.parsePage();
+        List<Book> books = getExpectedData();
 
-        expectedBookPage.open();
+        openExpectedPage();
 
-        expectedBook = expectedBookPage.parseBookDetails();
+        saveExpectedData();
 
-        Assert.assertTrue(books.contains(expectedBook), "The expected book is not found in the list");
-
+        IsInList(books, expectedBook);
     }
+
+    @Step("Open main page")
+    public void openMainPage() {
+        mainPage.open();
+    }
+
+    @Step("Set parameter")
+    public void setParameter(){
+        mainPage.setParameter("Books");
+    }
+
+    @Step("Enter word")
+    public void enterWord(){
+        mainPage.setParameter("Books");
+    }
+
+
+    @Step("Click on search button")
+    public void clickOnSearchBtn(){
+        mainPage.clickOnSearchBtn();
+    }
+
+    @Step("Getting expected data")
+    public List<Book> getExpectedData(){
+        List<Book> books = mainPage.parsePage();
+        return books;
+    }
+
+    @Step("Opening page of expected book")
+    public void openExpectedPage(){
+        expectedBookPage.open();
+    }
+
+    @Step("Saving data from a page of expected book")
+    public void saveExpectedData(){
+        expectedBook = expectedBookPage.parseBookDetails();
+    }
+
+    @Step("Checking if the book is in list")
+    public void IsInList(List<Book> books, Book book){
+        Assert.assertTrue(books.contains(expectedBook), "The expected book is not found in the list");
+    }
+
     //кінець
     @AfterMethod
+    @Step("Close WebDriver")
     public void teardown() {
         if (driver != null) {
             driver.quit();
